b172af48c294930fb2d9bb5e8102273e534d2057
Dakshkumar07 <74961250+Dakshkumar07@users.noreply.github.com>
2021-01-02 15:51:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# AngryBird3Constrained
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
---
+++ SlingShot.js
@@ -0,0 +1,30 @@
+class SlingShot {
+        constructor(bodyA,pointB){
+       var option ={
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness :0.04,
+            lenght :5 
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(option);
+        World.add(world,this.sling);
+        
+        }
+
+         fly(){
+         this.sling.bodyA = null              
+         }
+
+        display(){
+            if(this.sling.bodyA){ 
+            var pointA = this.sling.bodyA.position
+            var pointB = this.pointB
+          // push();
+            strokeWeight(4);
+            line(pointA.x,pointA.y,pointB.x,pointB.y)
+           // pop();
+            }
+        }
+
+}
